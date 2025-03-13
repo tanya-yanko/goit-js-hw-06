bbdb12ed1ff4c3c6c8d236bfef6242331b95ea85
tanya-yanko <yanko200662@gmail.com>
2025-03-13 21:29:13 +0200

    done

---
+++ js/task-1.js
@@ -0,0 +1,31 @@
+const customer = {
+    username: "Mango",
+    balance: 24000,
+    discount: 0.1,
+    orders: ["Burger", "Pizza", "Salad"],
+    
+    getBalance() {
+      return this.balance;
+    },
+    getDiscount() {
+      return this.discount;
+    },
+    setDiscount(value) {
+      this.discount = value;
+    },
+    getOrders() {
+      return this.orders;
+    },
+    addOrder(cost, order) {
+      this.balance -= cost - cost * this.discount;
+      this.orders.push(order);
+    },
+    // Change code above this line
+  };
+  
+  customer.setDiscount(0.15);
+  console.log(customer.getDiscount()); 
+  customer.addOrder(5000, "Steak");
+  console.log(customer.getBalance()); 
+  console.log(customer.getOrders()); 
+  
